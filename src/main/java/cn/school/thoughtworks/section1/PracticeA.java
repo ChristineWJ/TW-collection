a8c3e3a0b8944a9d6873e73c80e24525f7431efa
WenJieCao <wjcao@thoughtworks.com@twcns-MacBook-Pro-3.local>
2018-04-01 01:09:35 +0800

    modified A and B

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,19 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.List;
+import java.util.*;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        //实现练习要求，并改写该行代码
+        List<String> rst = new ArrayList<>();
+        String s;
+        for (int i=0;i<collection1.size();i++) {
+            s=collection1.get(i);
+            if(collection2.contains(s)&&!rst.contains(s)){
+                rst.add(s);
+            }
+        }
+        return new ArrayList<>(rst);
     }
+
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,23 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        //实现练习要求，并改写该行代码
+        List<String> rst = new ArrayList<>();
+        String s;
+        List<String> x;
+        for (int j = 0; j < collection2.size(); j++) {
+            x = collection2.get(j);
+            for (int i = 0; i < collection1.size(); i++) {
+                s = collection1.get(i);
+                if (x.contains(s) && !rst.contains(s)) {
+                    rst.add(s);
+                }
+            }
+        }
+        return new ArrayList<>(rst);
     }
 }
