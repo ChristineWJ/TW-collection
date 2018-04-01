dadeaa769e4e0af5669f832ef557d4b6894f6bfd
ChristineWJ <christinewj1102@gmail.com>
2018-04-01 23:51:21 +0800

    add PracticeA and PracticeB in the section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,36 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.lang.String;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Iterator iterator = collectionA.keySet().iterator();
+        String k;
 
-        return null;
+        List<List<String>> templst = new ArrayList<>();
+        Collection<List<String>> values = object.values();
+        Iterator<List<String>> iter = values.iterator();
+        while (iter.hasNext()) {
+            templst.add(iter.next());
+        }
+
+        ArrayList<String> collection2 = new ArrayList<>();
+        for (int i = 0; i < templst.size(); i++) {
+            for (int j = 0; j < templst.get(i).size(); j++) {
+                collection2.add(templst.get(i).get(j));
+            }
+        }
+
+
+        while (iterator.hasNext()) {
+            k = (String) iterator.next();
+            if (collection2.contains(k)) {
+                collectionA.put(k, collectionA.get(k) - 1);
+            }
+        }
+
+        return collectionA;
     }
 }
--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,38 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Iterator iterator = collectionA.keySet().iterator();
+        String k;
 
-        return null;
+        List<List<String>> templst = new ArrayList<>();
+        Collection<List<String>> values = object.values();
+        Iterator<List<String>> iter = values.iterator();
+        while (iter.hasNext()) {
+            templst.add(iter.next());
+        }
+
+        ArrayList<String> collection2 = new ArrayList<>();
+        for (int i = 0; i < templst.size(); i++) {
+            for (int j = 0; j < templst.get(i).size(); j++) {
+                collection2.add(templst.get(i).get(j));
+            }
+        }
+
+        int val;
+
+        while (iterator.hasNext()) {
+            k = (String) iterator.next();
+            if (collection2.contains(k)) {
+                val=collectionA.get(k);
+                if(val>=3)
+                collectionA.put(k, val - val/3);
+            }
+        }
+
+        return collectionA;
     }
 }
