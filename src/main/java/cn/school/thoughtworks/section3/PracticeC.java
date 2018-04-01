d1f897dc03c152830802d8dc362158f9129e5a5d
ChristineWJ <christinewj1102@gmail.com>
2018-04-02 00:12:55 +0800

    add PracticeC and PracticeD in the section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,27 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        Map<String, Integer> collection3 = new HashMap<>();
+
+        Iterator<String> iter = collectionA.iterator();
+        String h="";
+        while(iter.hasNext()){
+            h = iter.next();
+            if(collection3.get(h)==null){
+                collection3.put(h,1);
+            }else{
+                int oldValaue=collection3.get(h);
+                collection3.put(h,oldValaue+1);
+            }
+        }
+        PracticeB practiceB=new PracticeB();
+        return practiceB.createUpdatedCollection(collection3,object);
+
+
     }
 }
--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,16 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        cn.school.thoughtworks.section2.PracticeB practice = new cn.school.thoughtworks.section2.PracticeB();
+        PracticeB practiceB = new PracticeB();
+        return practiceB.createUpdatedCollection(practice.countSameElements(collectionA), object);
 
-        return null;
     }
 }
